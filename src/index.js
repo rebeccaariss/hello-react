486b0d51745e55ddc818826ef6b43dded94b73d6
Rebecca Ariss <rebeccamarionruth@gmail.com>
2023-10-02 09:42:45 -0400

    Make input a controlled component; improve UX by displaying 'Hello' only when there is text in the input field (using short circuit evaluation)

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ const Button = (props) => {
 };
 
 const Application = () => {
-  const [ name, setName ] = useState("Rebecca");
+  const [ name, setName ] = useState("");
 
   const reset = () => {
     console.log("reset");
@@ -21,9 +21,13 @@ const Application = () => {
 
   return (
     <main>
-      <input placeholder="Type your name"></input>
+      <input 
+        value={name}
+        onChange={(event) => setName(event.target.value)}
+        placeholder="Type your name"
+      />
       <Button reset={reset} />
-      <h1>Hello {name}</h1>
+      {name && <h1>Hello {name}</h1>}
     </main>
   );
 };
